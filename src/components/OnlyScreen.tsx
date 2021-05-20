5d07788c2f4796ba5f065e72803e70ead9a5f630
Lucas Ciruzzi <hi@lshi.ru>
2021-05-19 19:32:16 -0700

    :art: better styles for height.

--- src/components/OnlyScreen.tsx
+++ src/components/OnlyScreen.tsx
@@ -4,4 +4,9 @@ import type { WrapperComponent } from "../types/WrapperComponent";
 export const OnlyScreen: WrapperComponent = ({
 	class: className,
 	...properties
-}) => <div class={classnames("print:hidden", className)} {...properties} />;
+}) => (
+	<div
+		class={classnames("print:hidden h-stretch", className)}
+		{...properties}
+	/>
+);
