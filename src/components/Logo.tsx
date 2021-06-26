4e2595be3cd4ad03646c1f4d7b63eed604c7781c
Lucas Ciruzzi <hi@lshi.ru>
2021-06-26 03:43:28 -0700

    :art: new logo and new photo.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -10,13 +10,15 @@ export const Logo: WrapperComponent = ({ class: className, ...properties }) => (
 		)}
 		{...properties}
 	>
-		<svg class="w-full" viewBox="0 0 8 8">
+		<svg viewBox="0 0 4 4" class="w-full">
+			<circle class="fill-current" cx="2" cy="2" r="2" />
 			<path
-				class="fill-current"
-				// eslint-disable-next-line max-len
-				d="M3.386 1.064A3.002 3.002 0 001 4c0 .322.052.637.15.936L.536 6h1.23a2.997 2.997 0 004.468 0h1.23L6.85 4.936a2.997 2.997 0 00-.73-3.056 3.002 3.002 0 00-1.506-.816L4 0l-.614 1.064zM0 4c0-2.208 1.792-4 4-4s4 1.792 4 4-1.792 4-4 4-4-1.792-4-4zm4-3l2.598 4.5H1.402L4 1zm0 2l.87 1.5H3.13L4 3z"
-				fill-rule="evenodd"
+				class="text-white dark:text-black stroke-current"
+				d="M2 1l.865 1.5h-1.73z"
+				stroke-linecap="round"
+				stroke-linejoin="round"
 			/>
+			<path class="fill-current" d="M2 1l.865 1.5h-1.73z" />
 		</svg>
 	</div>
 );
